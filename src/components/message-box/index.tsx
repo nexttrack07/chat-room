54ce972178de6d4436d4d329573bb6ddb9d42dbb
Faadhil Hakim <faadhil1991@Faadhils-MacBook-Pro.local>
2020-07-30 14:28:46 -0500

    feat: change label name

--- src/components/message-box/index.tsx
+++ src/components/message-box/index.tsx
@@ -7,19 +7,20 @@ import {
   Button,
 } from 'semantic-ui-react'
 import { useDispatch, useSelector } from 'react-redux'
-import { User } from 'services/user-service'
+// import { User } from 'services/user-service'
 import { fetchUsers } from './user.slice'
 import { addMessage } from 'components/chat-log/chat-log.slice'
-import { RootState } from 'components/app/store'
+// import { RootState } from 'components/app/store'
 import styles from './message-box.module.css'
 
 function MessageBox() {
   const [message, setMessage] = React.useState('')
-  const users = useSelector((state: RootState) => state.users)
-  const [selectedUser, setSelectedUser] = React.useState<User>({
-    name: 'Ben',
-    avatar: 'https://react.semantic-ui.com/images/avatar/small/elliot.jpg',
-  })
+  const [labelName, setLabelName] = React.useState('')
+  // const users = useSelector((state: RootState) => state.users)
+  // const [selectedUser, setSelectedUser] = React.useState<User>({
+  //   name: 'Ben',
+  //   avatar: 'https://react.semantic-ui.com/images/avatar/small/elliot.jpg',
+  // })
   const dispatch = useDispatch()
 
   React.useEffect(() => {
@@ -27,38 +28,35 @@ function MessageBox() {
   }, [dispatch])
 
   function handleSendClick() {
-    if (selectedUser) {
-      dispatch(addMessage({ sender: selectedUser, text: message }))
+    if (message) {
+      dispatch(addMessage({ sender: { name: labelName }, text: message }))
     }
 
     setMessage('')
   }
 
-  function handleMenuChange(
-    event: React.SyntheticEvent,
-    data: DropdownItemProps
-  ) {
-    const usr = users.find((user) => user.name === data.text)
-    if (usr) {
-      setSelectedUser(usr)
-    }
+  // function handleMenuChange(
+  //   event: React.SyntheticEvent,
+  //   data: DropdownItemProps
+  // ) {
+  //   const usr = users.find((user) => user.name === data.text)
+  //   if (usr) {
+  //     setSelectedUser(usr)
+  //   }
+  // }
+
+  function handleLabelChange({
+    target: { value },
+  }: {
+    target: { value: any }
+  }) {
+    setLabelName(value)
   }
 
   return (
     <div data-testid='message-box' className={styles.messageWrapper}>
-      <Label className={styles.messageLabel}>
-        <Dropdown closeOnChange text={selectedUser?.name} simple>
-          <Dropdown.Menu>
-            {users.map((user) => (
-              <Dropdown.Item
-                key={user.name}
-                text={user.name}
-                onClick={handleMenuChange}
-              />
-            ))}
-          </Dropdown.Menu>
-        </Dropdown>
-      </Label>
+      <Input onChange={handleLabelChange} />
+      <Label className={styles.messageLabel}>{labelName}</Label>
       <div className={styles.messageInputWrapper}>
         <Input
           value={message}
